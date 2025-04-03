900e24a8392de0173854413062504ca9eae4d524
Nicoli <103062064+ItsNiSantana@users.noreply.github.com>
2025-04-03 11:15:43 -0300

    Update script.js

--- aula1_arquivo0/script.js
+++ aula1_arquivo0/script.js
@@ -1,4 +1,16 @@
+const options = { style: 'currency', currency: 'BRL', minimumFractionDigits: 2, maximumFractionDigits: 3 }
+const formatNumber = new Intl.NumberFormat('pt-BR', options)
 valoryenes = prompt("digite um valor em yenes")
 
 umyene = 0.0386;
-alert("R$" + valoryenes * umyene)
+alert(formatNumber.format(valoryenes * umyene))
+
+valordolares = prompt("digite um valor em doláres")
+
+umdolar = 5.7450;
+alert(formatNumber.format(valordolares * umdolar))
+
+valoreuros = prompt("digite um valor em euros")
+
+umeuro = 6.2289;
+alert(formatNumber.format(valoreuros * umeuro))
